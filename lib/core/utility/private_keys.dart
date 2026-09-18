292b37688d94a8166610f0017fee4e291e5a6a74
Neura Tech AI <samarthpathak62@gmail.com>
2026-09-18 12:31:09 +0530

    Update private_keys.dart

--- lib/core/utility/private_keys.dart
+++ lib/core/utility/private_keys.dart
@@ -1,2 +1,2 @@
-const String notificationKey = 'key=unused-fcm-server-key';
+const String notificationKey = 'BMZCkeZGMGgyUMES1D9yDQ2sfwJ50fJ8ZS6pCr43p-SD-nqH8y3SxiVo-fzRalvDrOdSud9KD8Eg3OvKgvHfKMA';
 const String agoraAppId = '';
